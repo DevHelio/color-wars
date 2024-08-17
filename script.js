07b2ae25ac82a1761eb5f95ef16f8156807d30e6
TruFriendsNeverDie <heliodev@outlook.com>
2024-08-18 01:45:05 +0200

    working on the first implementation, using DOM objects instead of canvas (my first approach)

--- script.js
+++ script.js
@@ -15,5 +15,38 @@ document.body.style.borderSizing = 'border-box';
 document.body.style.backgroundColor = 'black';
 document.body.style.display = 'flex';
 
+//setting up board
 const b = document.getElementById('board');
+let boardSize = 7;
+b.style.margin = '0';
+b.style.padding = '0';
+b.style.borderSizing = 'border-box';
+b.style.display = 'flex';
+b.style.flexDirection = 'column';
+b.style.justifyContent = 'center';
+b.style.alignItems = 'center';
+b.style.flex = '1';
+b.style.aspectRatio = '1/1';
+
+for (let i=0; i<boardSize; i++) {
+	let row = document.createElement('div');
+	b.appendChild(row);
+	row.classList.add(`row-${i}`);
+	row.style.margin = '0';
+	row.style.padding = '0';
+	row.style.borderSizing = 'border-box';
+	row.style.display = 'flex';
+	row.style.flexDirection = 'row';
+	row.style.justifyContent = 'center';
+	row.style.alignItems = 'center';
+	row.style.flex = '1';
+
+
+	for (let j=0; j<boardSize; j++)  {
+		let cell = document.createElement('button');
+		row.appendChild(cell);
+		cell.classList.add(`cell-${i}-${j}`);
+		cell.innerHTML = '&nbsp;';
+	}
+}
 
